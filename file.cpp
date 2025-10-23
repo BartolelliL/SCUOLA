41576feb25fbe7f48e432470fefda6465181613d
studente <pignarberi.matteo.stud@itisgaluileiroma.it>
2025-10-23 09:16:46 +0200

    commit

--- file.cpp
+++ file.cpp
@@ -3,8 +3,28 @@
 using namespace std;
 
 int main() {
+    int numeri[10];
 
-    cout << "Hello World" << endl;
+    for (int i = 0; i < 10; i++) { 
+        cout << "Inserisci un numero intero: ";
+        cin >> numeri[i];
+    }
+
+    for (int i = 0; i < 10 - 1; i++) {
+        for (int j = 0; j < 10 - i - 1; j++) {
+            if (numeri[j] > numeri[j + 1]) {
+                int temp = numeri[j];
+                numeri[j] = numeri[j + 1];
+                numeri[j + 1] = temp;
+            }
+        }
+    }
+    
+    cout << "Array ordinato: ";
+    for (int i = 0; i < 10; i++) {
+        cout << numeri[i] << " ";
+    }
+    cout << endl;
+
     return 0;
-
 }
