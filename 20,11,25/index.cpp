be01289b5c5cd70ed4a3bcdbf9e42edfb061299a
studente <pignarberi.matteo.stud@itisgaluileiroma.it>
2025-11-20 09:08:38 +0100

    .

--- 20,11,25/index.cpp
+++ 20,11,25/index.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main() {
 
     int km;
-    double costo_per_km = 0.21;
+    double costo_per_km = 2.21;
     double totale;
 
     cout << "Inserisci i kilometri da percorrere: ";
     cin >> km;
     totale = km * costo_per_km;
     cout << "Il totale da pagare e': " << totale << " euro.\n";
-
+    system("pause");
     return 0;
-    
+
 }
