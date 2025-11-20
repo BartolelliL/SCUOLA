0110bd161dfe1d415a2b10f06cb6aa8b09868f6e
studente <pignarberi.matteo.stud@itisgaluileiroma.it>
2025-11-20 09:27:21 +0100

    .

--- 20,11,25/index.cpp
+++ 20,11,25/index.cpp
@@ -2,17 +2,41 @@
 
 using namespace std;
 
+int scontrino() {
+
+    int fotocopie_in_bianco_e_nero;
+    int fotocopie_a_colori;
+    int rilegatura;
+
+    float costo_totale;
+    float costo_fotocopie_in_bianco_e_nero = 0.10;
+    float costo_fotocopie_a_colori = 0.80;
+    float costo_rilegatura = 1.30;
+
+    cout << "Salve, quante fotocopie in bianco e nero vuole stampare? ";
+    cin >> fotocopie_in_bianco_e_nero;
+
+    cout << "Quante fotocopie a colori vuole stampare? ";
+    cin >> fotocopie_a_colori;
+
+    cout << "Vuole la rilegatura? (1 per si, 0 per no) ";
+    cin >> rilegatura;
+
+    costo_totale = (fotocopie_in_bianco_e_nero * costo_fotocopie_in_bianco_e_nero) + (fotocopie_a_colori * costo_fotocopie_a_colori);
+
+    if (rilegatura == 1) {
+        costo_totale += costo_rilegatura;
+    }
+
+    return costo_totale;
+}
+
 int main() {
 
-    int km;
-    double costo_per_km = 2.21;
-    double totale;
+    float costo_totale = scontrino();
 
-    cout << "Inserisci i kilometri da percorrere: ";
-    cin >> km;
-    totale = km * costo_per_km;
-    cout << "Il totale da pagare e': " << totale << " euro.\n";
-    system("pause");
+    cout << "Il costo totale delle fotocopie e': " << costo_totale << " euro." << endl;
+
     return 0;
 
 }
